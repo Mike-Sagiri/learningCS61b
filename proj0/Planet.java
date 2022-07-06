9337edc948190ee9fd1559d3cfc1884ff17ef5cb
Mike-Sagiri <dangkuchangge@gmail.com>
2022-07-06 17:28:05 +0800

    finished

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static final double Gconst = 6.67e-11;
+    private static final double Gconst = 6.67e-11;
     /** Constructors
      *
      * @param xP initial xxPos
